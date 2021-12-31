6e68970c69730f4a1ffd0df3158b672ea94e887a
olivrrrrr <oliverekwalla@gmail.com>
2021-12-31 02:06:37 +0000

    part1 completed

---
+++ part1/unicafe/src/components/StatisticTitle.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function StatisticTitle() {
+    return (
+        <>
+         <h1>statistics</h1>   
+        </>
+    )
+}
+
+export default StatisticTitle
---
+++ part1/unicafe/src/components/Title.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Title() {
+    return (
+        <>
+          <h1>give feedback</h1> 
+        </>
+    )
+}
+
+export default Title
