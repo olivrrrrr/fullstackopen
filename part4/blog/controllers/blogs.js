f4f934e4b02ced1d25042bb6e70212a0a23ce758
Oliver Ekwalla <oliverekwalla@gmail.com>
2022-02-04 14:19:00 +0000

    Exercises 4.8 - 4.12

--- part4/blog/controllers/blogs.js
+++ part4/blog/controllers/blogs.js
@@ -1,23 +1,57 @@
-const notesRouter = require('express').Router()
-const Blog = require('../models/blog')
+const notesRouter = require("express").Router();
+const { response } = require("express");
+const Blog = require("../models/blog");
 
-notesRouter.get('/', (request, response)=>{
-    Blog
-        .find({})
-        .then(blogs=>{
-            response.json(blogs)
-        })
-})
+notesRouter.get("/", async (request, response) => {
+  const blogs = await Blog.find({});
+  response.json(blogs);
+});
 
-notesRouter.post('/', (request, response, next)=>{
-    const blog = new Blog(request.body)
+notesRouter.get("api/blogs/:id", async (request, response) => {
+  const blog = await Blog.findById(request.params.id);
+  response.json(blog);
+});
 
-    blog
-        .save()
-        .then(result =>{
-            response.status(201).json(result)
-        })
+notesRouter.post("/", async (request, response, next) => {
+  const body = request.body;
 
-})
+  const blog = new Blog({
+    author: body.author,
+    title: body.title,
+    url: body.url,
+    likes: body.likes || 0,
+  });
 
-module.exports = notesRouter
+  // const blog = blog.save().then((result) => {
+  //   response.status(201).json(result);
+  // });
+
+  // console.log(blog["author"] === undefined);
+
+  try {
+    if (!(blog["title"] === undefined) || !(blog["url"] === undefined)) {
+      const savedBlog = await blog.save();
+      response.status(201).json(savedBlog.toJSON());
+    } else {
+      response.status(400).json({ error: error.message });
+    }
+    // const savedBlog = await blog.save();
+    // response.status(201).json(savedBlog);
+  } catch (exception) {
+    next(exception);
+  }
+});
+
+// const errorHandler = (error, request, res, next) => {
+//   console.error(error.message);
+
+//   if (error.name === "CastError") {
+//     return res.status(400).send({ error: "malformatted id" });
+//   }
+
+//   next(error);
+// };
+
+// app.use(errorHandler);
+
+module.exports = notesRouter;
--- part4/blog/models/blog.js
+++ part4/blog/models/blog.js
@@ -1,19 +1,18 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const blogSchema = new mongoose.Schema({
-    author: String, 
-    title: String, 
-    url: String, 
-    likes: Number
-})
+  author: String,
+  title: String,
+  url: String,
+  likes: Number,
+});
 
-blogSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
-})
+blogSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    // delete returnedObject._id
+    delete returnedObject.__v;
+  },
+});
 
-module.exports = mongoose.model('Blog', blogSchema)
-
+module.exports = mongoose.model("Blog", blogSchema);
